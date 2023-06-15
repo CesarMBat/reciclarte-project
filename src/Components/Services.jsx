07a03a6f16f4836b56c30f6cfb4e1d05fb30a037
CésarMBat <cesar.morsoleto@gmail.com>
2023-06-15 19:04:06 -0300

    Adição de classNames para as lis do component services

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -10,31 +10,31 @@ const Services = () => {
                 <section className="section-materials m1">
                     <h1>Matérias-primas</h1>
                     <ul className='ul-services'>
-                      <li>Garrafas de vidro</li>
-                      <li>Retalhos de tecido</li>
+                      <li className='li-service'>Garrafas de vidro</li>
+                      <li className='li-service'>Retalhos de tecido</li>
                     </ul>
                 </section>
                 <section className="section-materials m2">
                     <h1>Materiais de consumo</h1>
                     <ul className='ul-services'>
-                      <li>Kit do produtor</li>
-                      <li>Tintas, colas e vernizes</li>
-                      <li>Ferramentas de corte e acabamento</li>
+                      <li className='li-service'>Kit do produtor</li>
+                      <li className='li-service'>Tintas, colas e vernizes</li>
+                      <li className='li-service'>Ferramentas de corte e acabamento</li>
                     </ul>
                 </section>
                 <section className="section-materials m3">
                     <h1>Produção</h1>
                     <ul className='ul-services'>
-                      <li>Coleta dos materiais</li>
-                      <li>Higienização</li>
-                      <li>Corte e acabamento</li>
-                      <li>Práticas de reciclagem</li>
+                      <li className='li-service'>Coleta dos materiais</li>
+                      <li className='li-service'>Higienização</li>
+                      <li className='li-service'>Corte e acabamento</li>
+                      <li className='li-service'>Práticas de reciclagem</li>
                     </ul>
                 </section>
                 <section className="section-materials m4">
                     <h1>Prazo de produção</h1>
                     <ul className='ul-services'>
-                      <li>Média de 2 dias</li>
+                      <li className='li-service'>Média de 2 dias</li>
                     </ul>
                 </section>
             </main>{' '}
