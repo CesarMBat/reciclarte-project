eca329eb1383eea1fe896865036a5ec82d07d1d3
CesarMBat <cesar.morsoleto@gmail.com>
2023-07-05 15:56:10 -0300

    Troquei para NavLink

--- src/Components/Nav-Mobile.jsx
+++ src/Components/Nav-Mobile.jsx
@@ -1,16 +1,19 @@
 //import React from 'react'
 import { fallDown as Menu } from 'react-burger-menu'
 import './nav.css'
+import { NavLink, Outlet } from 'react-router-dom'
 
 function Nav_Mobile() {
   return (
     <div className='nav-mobile'>
       <Menu id={ "sidebar" } className={ "my-menu" } width={'100%'}>
-        <a id="home" className="menu-item" href="/">Página Inicial</a>
-        <a id="about" className="menu-item" href="/historia">Nossa História</a>
-        <a id="contact" className="menu-item" href="/quem_somos">Quem Somos</a>
-        <a id="contact" className="menu-item" href="/servicos">Nossos Serviços</a>
+        <NavLink id="home" className="menu-item" to="/">Página Inicial</NavLink>
+        <NavLink id="about" className="menu-item" to="/historia">Nossa História</NavLink>
+        <NavLink id="contact" className="menu-item" to="/quem_somos">Quem Somos</NavLink>
+        <NavLink id="contact" className="menu-item" to="/servicos">Nossos Serviços</NavLink>
       </Menu>
+      <Outlet />
+
     </div>
   )
 }
