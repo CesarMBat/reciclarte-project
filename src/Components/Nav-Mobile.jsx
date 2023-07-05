9e4ba9f6152422fc128dbe254a804d5bb6d563ea
CesarMBat <cesar.morsoleto@gmail.com>
2023-07-05 15:49:38 -0300

    Burger menu in development

--- src/Components/Nav-Mobile.jsx
+++ src/Components/Nav-Mobile.jsx
@@ -1,15 +1,15 @@
 //import React from 'react'
-import { slide as Menu } from 'react-burger-menu'
-
+import { fallDown as Menu } from 'react-burger-menu'
+import './nav.css'
 
 function Nav_Mobile() {
   return (
-    <div>
-      <Menu>
-        <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/about">About</a>
-        <a id="contact" className="menu-item" href="/contact">Contact</a>
-        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+    <div className='nav-mobile'>
+      <Menu id={ "sidebar" } className={ "my-menu" } width={'100%'}>
+        <a id="home" className="menu-item" href="/">Página Inicial</a>
+        <a id="about" className="menu-item" href="/historia">Nossa História</a>
+        <a id="contact" className="menu-item" href="/quem_somos">Quem Somos</a>
+        <a id="contact" className="menu-item" href="/servicos">Nossos Serviços</a>
       </Menu>
     </div>
   )
