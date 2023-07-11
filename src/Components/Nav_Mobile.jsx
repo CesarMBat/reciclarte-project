659d2557567ca0e1afd956cc7648984c278a8b5a
CesarMBat <cesar.morsoleto@gmail.com>
2023-07-11 13:23:54 -0300

    Header mobile atualizado

--- src/Components/Nav_Mobile.jsx
+++ src/Components/Nav_Mobile.jsx
@@ -9,7 +9,7 @@ function Nav_Mobile() {
   return (
     <div className='nav-mobile'>
       <img src={MENU} className='menu-svg' />
-      <Menu id={ "slide" } className={ "my-menu" } width={'100%'} height={'10%'}>
+      <Menu id={ "slide" } className={ "my-menu" } width={'100%'}>
         <NavLink id="home" className="menu-item mi1" to="/">Página Inicial</NavLink>
         <NavLink id="about" className="menu-item" to="/historia">Nossa História</NavLink>
         <NavLink id="contact" className="menu-item" to="/quem_somos">Quem Somos</NavLink>
