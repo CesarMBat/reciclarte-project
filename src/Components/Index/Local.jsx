39bfb8f25826e3022c68625e1e27c3f535a9bd74
CésarMBat <cesar.morsoleto@gmail.com>
2023-07-10 22:00:30 -0300

    Marker added

--- src/Components/Index/Local.jsx
+++ src/Components/Index/Local.jsx
@@ -9,13 +9,7 @@ const Local = () => {
     lat: -23.55395905109023,
     lng: -46.687473872690674,
   };
-  const svgIcon = {
-    path: "M12 2C6.48 2 2 6.48 2 12c0 4.41 3.58 8 8 8s8-3.59 8-8c0-5.52-4.48-10-10-10zm0 14c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm0-7c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z",
-    fillColor: 'blue',
-    fillOpacity: 1.0,
-    strokeWeight: 0,
-    scale: 1.5
-  };
+  
   return (
     <div>
       <main className="main-local">
@@ -37,7 +31,7 @@ const Local = () => {
             >
               <Marker
                 position={defaultCenter}
-                icon={svgIcon}
+                icon={"http:// labs.google.com/ridefinder/images/mm_20_green.png"}
               >
               </Marker>
             </GoogleMap>
